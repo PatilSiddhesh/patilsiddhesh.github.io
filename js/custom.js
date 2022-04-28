dbc12c204f535c4ca933e5f94c0a253fe3894e0c
Sid Patil <Siddhesh.s.patil@gmail.com>
2022-04-28 17:16:16 +0200

    Adds article on Monet and M3-compose

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-    if( navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i)|| navigator.userAgent.match(/Windows Phone/i)) {
-        if(window.location.href != "https://siddroid.com/" && window.location.href != "http://localhost:1313/" && window.location.href != "http://siddroid.com/") {
-        snapTop();
-    }
- }
+//     if( navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i)|| navigator.userAgent.match(/Windows Phone/i)) {
+//         if(window.location.href != "https://siddroid.com/" && window.location.href != "http://localhost:1313/" && window.location.href != "http://siddroid.com/") {
+//         snapTop();
+//     }
+//  }
 };
 
 function snapTop() {
