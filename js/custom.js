d4b836280b5d4b8812cf61815b7177eda93774bd
Sid Patil <Siddhesh.s.patil@gmail.com>
2023-06-30 00:30:33 +0200

    Changes to landing page

--- js/custom.js
+++ js/custom.js
@@ -20,4 +20,19 @@ function copyToClipboard(toCopy) {
     document.execCommand("copy");
     $temp.remove();
 }
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 3000); // Change image every 2 seconds
+// }
   
